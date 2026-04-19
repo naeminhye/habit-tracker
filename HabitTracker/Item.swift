de1c614a6eab942a409e2c2d200d8664c5be6028
Jacey N <minhhieu.eng2@gmail.com>
2026-04-19 12:38:41 +0700

    Initial Commit

---
+++ HabitTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HabitTracker
+//
+//  Created by JaceyNguyen on 19/04/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
